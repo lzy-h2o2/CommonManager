f6c15a5dc15484fa117b0832ad19ee1b818cb8c5
lazy <zylu@seeiner.com>
2018-10-22 15:45:27 +0800

    add logic for widgets of clear EditText

---
+++ app/src/main/java/com/zndroid/demo/funcs/widgets/FuncWidgetsActivity.java
@@ -0,0 +1,15 @@
+package com.zndroid.demo.funcs.widgets;
+
+import android.os.Bundle;
+
+import com.zndroid.demo.BaseActivity;
+import com.zndroid.demo.R;
+
+public class FuncWidgetsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.funcs_widgets_layout);
+    }
+}
